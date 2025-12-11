cc3277377d4014916c172eca30196e101ef15ebf
MatheusMachad00 <matheus_m_machado@hotmail.com>
2025-12-11 16:16:45 -0300

    Django - Third  Lesson completed

--- LESSON_3/myproject/posts/models.py
+++ LESSON_3/myproject/posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=75)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
+
+
+# max_length=75 => tamanho máximo da string
+# auto_now_add=True => adiciona uma data automaticamente
